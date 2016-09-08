bb9873c05047366daf38ed346c6137e362467be8
ryan_p_larson <ryan_p_larson@github.com>
2016-09-08 00:33:14 -0500

    Remove url list

--- src/download-cfb.sh
+++ src/download-cfb.sh
@@ -1,5 +1,6 @@
 # College Football data download script
-
+#
+# Output should be a folder with cfb from
 # Repole has data from 1978-2014
 # http://www.repole.com/sun4cast/data.html
 
@@ -7,17 +8,15 @@
 mkdir -p cfb;
 
 # download all of the data files
-wget -i cfb-urls -U "Opera";
+#wget -i cfb-urls -U "Opera";
+wget http://www.repole.com/sun4cast/stats/cfb2014lines.csv http://www.repole.com/sun4cast/stats/cfblines.zip http://www.repole.com/sun4cast/stats/bowl2014lines.csv http://www.repole.com/sun4cast/stats/bowllines.zip -U "Opera";
 
 # unzip only the csv files
-unzip bowllines.zip -x *.xml -d cfb -u --quiet;
-unzip cfblines.zip -x *.xml -d cfb -u --quiet;
+unzip bowllines.zip -x *.xml -d cfb -u -q --quiet;
+unzip cfblines.zip -x *.xml -d cfb -u -q;
 
 # remove the zips
 rm *.zip;
 
-# move the csv to the data file
+# move the csv to the data folder
 mv *.csv cfb;
-
-# and finally move the cfb folder to the data folder
-mv cfb ../data/in;

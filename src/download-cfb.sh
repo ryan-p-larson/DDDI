6d83e82bdff54ab753e4b3c968e7598d4d017274
ryan_p_larson <ryan_p_larson@github.com>
2016-09-08 00:21:56 -0500

    Script to download 1976-2014 CFB data using Wget and Unzip

--- src/download-cfb.sh
+++ src/download-cfb.sh
@@ -1,2 +1,23 @@
-# College Football data
+# College Football data download script
+
 # Repole has data from 1978-2014
+# http://www.repole.com/sun4cast/data.html
+
+# Create a folder to hold all the data
+mkdir -p cfb;
+
+# download all of the data files
+wget -i cfb-urls -U "Opera";
+
+# unzip only the csv files
+unzip bowllines.zip -x *.xml -d cfb -u --quiet;
+unzip cfblines.zip -x *.xml -d cfb -u --quiet;
+
+# remove the zips
+rm *.zip;
+
+# move the csv to the data file
+mv *.csv cfb;
+
+# and finally move the cfb folder to the data folder
+mv cfb ../data/in;
